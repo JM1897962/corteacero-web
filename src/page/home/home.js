c393f4d768559428a305a466350db53ba0a7d393
JM <1897962.jmmr@gmail.com>
2024-11-25 14:14:34 -0600

    Tercera actualizacion

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './home.css';
 import logo from '../../assets/logo.jpg'; // Asegúrate de que esta ruta sea correcta
+import Slider from "react-slick"; // Importar el componente Slider
+import imagen1 from '../../assets/imagen1.jpg';
+import imagen2 from '../../assets/imagen2.jpg';
+import imagen3 from '../../assets/imagen3.jpg';
 
 const Home = () => {
+    // Configuración del carrusel
+    const settings = {
+        dots: true, // Mostrar puntos de navegación
+        infinite: true, // Carrusel infinito
+        speed: 500, // Velocidad de transición
+        slidesToShow: 1, // Mostrar un slide a la vez
+        slidesToScroll: 1, // Desplazar un slide a la vez
+        autoplay: true, // Activar autoplay
+        autoplaySpeed: 3000, // Velocidad de autoplay
+    };
+
     return (
         <div className="contenedor-nosotros">
             <div className="bienvenida">
@@ -22,16 +37,30 @@ const Home = () => {
                 </div>
             </div>
 
+            {/* Carrusel de imágenes */}
+            <div className="carrusel">
+                <Slider {...settings}>
+                    <div>
+                        <img src={imagen1} alt="Imagen 1" />
+                    </div>
+                    <div>
+                        <img src={imagen2} alt="Imagen 2" />
+                    </div>
+                    <div>
+                        <img src={imagen3} alt="Imagen 3" />
+                    </div>
+                    {/* Agrega más imágenes según sea necesario */}
+                </Slider>
+            </div>
+
             {/* Barra inferior con elementos alineados */}
             <div className="barra-inferior">
                 <div className="opciones">
-                    {/* Opciones de navegación en la barra inferior */}
                     <span>Menú</span>
                     <span>Contáctanos</span>
                     <span>Síguenos</span>
                 </div>
                 <div className="copyright">
-                    {/* Texto de derechos de autor */}
                     Copyright ©2024 CorteAcero. Todos los Derechos Reservados.
                 </div>
             </div>
